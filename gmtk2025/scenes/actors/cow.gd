3b112828397df7d242f3c9c6a219ede68f5852e4
Kazoya - Zaid Mermam <mermamzaid@gmail.com>
2025-08-01 01:08:36 +0300

    cows moving yay
    cows now move in random directions yay
    
    changed "cow" from node2d to characterBody2D
    they also have a collider now

--- gmtk2025/scenes/actors/cow.gd
+++ gmtk2025/scenes/actors/cow.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 class_name Cow
 
 enum CowType {
@@ -10,13 +10,40 @@ enum CowType {
 
 @export var cow_type: CowType = CowType.UNDEFINED
 
+@export_range(0.0, 100.0) var move_speed: float = 50.0
+@export var time_between_moves: float = 2.0
+# ranges for the next random position 
+@export var cow_position_offset: int = 100
+var target_position: Vector2
+var waiting: bool = true
+var timer: float = 0.0
+
 func _ready() -> void:
 	cow_type = randi_range(1, 3)
 	$AnimatedSprite2D.modulate = Global.cow_type_to_color(cow_type)
-
-func _process(delta: float) -> void:
-	pass
-	# TODO: cow AI
+	_pick_new_target()
 
 func capture():
-	queue_free()
+	queue_free() # cow ded
+
+func _physics_process(delta: float) -> void:
+	if waiting:
+		timer -= delta
+		if timer <= 0.0:
+			_pick_new_target()
+	else:
+		var direction = (target_position - global_position).normalized()
+		velocity = direction * move_speed
+		move_and_slide()
+
+		# If cow is very close to target, stop and start waiting
+		if global_position.distance_to(target_position) <= 1.0:
+			velocity = Vector2.ZERO
+			waiting = true
+			timer = time_between_moves
+
+func _pick_new_target():
+	var rand_x = randf_range(position.x + cow_position_offset, position.x - cow_position_offset)
+	var rand_y = randf_range(position.y + cow_position_offset,position.y - cow_position_offset)
+	target_position = Vector2(rand_x, rand_y)
+	waiting = false
