5672217624361958b5ace3c51d3dc9fb7a986fc4
Kazoya - Zaid Mermam <mermamzaid@gmail.com>
2025-08-03 16:19:56 +0300

    better numbers

--- gmtk2025/scenes/score_manager.gd
+++ gmtk2025/scenes/score_manager.gd
@@ -49,42 +49,58 @@ func apply_lasso_result(pink_count: int, black_count: int, lasso_type: Cow.CowTy
 	var right_count = pink_count if lasso_type == Cow.CowType.PINK else black_count
 	var wrong_count = total - right_count
 	
-	var points_per_right_cow = (1 + right_count)
-	var points_per_wrong_cow = (2 * points_per_right_cow)
+	# Keep this base calculation simple
+	var points_per_right_cow = 10 # Base points for a correct cow
+	var penalty_for_wrong_cow = 20 # Base penalty for a wrong cow
 	
-	var final_point_diff = (
-		points_per_right_cow * right_count - 
-		points_per_wrong_cow * wrong_count
-	)
-	if final_point_diff > 0:
-		points_per_right_cow *= combo_multiplier 
-		points_per_wrong_cow *= combo_multiplier 
+	var base_point_diff = (points_per_right_cow * right_count) - (penalty_for_wrong_cow * wrong_count)
 	
-	if final_point_diff > 150:
-		gain_a_lot_point.emit()
+	var final_point_diff = base_point_diff # Start with the base difference
+	
+	if base_point_diff > 0:
+		# --- SUCCESSFUL CATCH ---
+		combo += 1 # <-- FIX #1: Actually increase the combo count!
+		
+		# Apply a multiplier that grows with the combo streak
+		var current_multiplier = 1.0 + (combo * 0.5) # Example: combo 1 = x1.5, combo 2 = x2.0, etc.
+		final_point_diff *= current_multiplier
+		
+		# Emit signals based on final calculated points
+		if final_point_diff > 150:
+			gain_a_lot_point.emit()
+		else:
+			gain_point.emit()
 		$CatchCowSound.play()
-	elif final_point_diff > 0:
-		gain_point.emit()
-		$CatchCowSound.play()
-	elif final_point_diff < 0 or (final_point_diff == 0 and total > 0):
+
+	elif base_point_diff < 0 or (base_point_diff == 0 and total > 0):
+		# --- FAILED CATCH ---
+		combo = 0 # <-- FIX #2: Reset the combo on a miss
 		lose_point.emit()
 		$MissCowSound.play()
 	
-	print(final_point_diff)
+	# Make sure points are integers for score and particles
+	final_point_diff = int(final_point_diff)
 	
-	energy += final_point_diff 
-	score += final_point_diff 
+	print("Points gained: ", final_point_diff, " | New Combo: ", combo)
+	
+	energy += final_point_diff
+	score += final_point_diff
 	score = max(0, score)
 	
-	if energy > max_energy:
-		combo_multiplier += 1
+	# Your "overcharge" logic could be a separate mechanic if you want
+	# if energy > max_energy:
+		# gain_special_power() 
 	
 	for cow in captured_cows:
 		var score_particle = score_particle_prefab.instantiate()
+		var particle_points = 0
 		if cow.cow_type == lasso_type:
-			score_particle.score = int(points_per_right_cow)
+			# Show the base points multiplied by the combo
+			particle_points = int(points_per_right_cow * (1.0 + (combo * 0.5)))
 		else:
-			score_particle.score = -int(points_per_wrong_cow)
+			particle_points = -penalty_for_wrong_cow
+			
+		score_particle.score = particle_points
 		add_child(score_particle)
 		score_particle.global_position = cow.global_position
 		score_particle.position.x -= 32
